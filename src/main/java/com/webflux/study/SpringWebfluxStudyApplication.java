e0ae2bc8a275b31a272b90eb3a23cdff27ed45a5
wellbell <uok02018496@naver.com>
2021-06-22 19:50:00 +0900

    initial commit

---
+++ src/main/java/com/webflux/study/SpringWebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.webflux.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webflux/study/SpringWebfluxStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webflux.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
